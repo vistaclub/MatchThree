f6004fb67fa177f945c941e8b03270b0352049d9
Jason Wong <jawong71@gmail.com>
2018-09-29 19:01:36 -0700

    first commit

---
+++ Project17/Ball.swift
@@ -0,0 +1,13 @@
+//
+//  Ball.swift
+//  Project17
+//
+//  Created by Jason Wong on 2018-09-29.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class Ball: SKSpriteNode {
+
+}
